6cb14063328051a2efe112f2b875d70dd60cb627
Thomas Hockaday <thomashockaday@intelligencefusion.co.uk>
2026-01-15 15:37:52 +0000

    Start drawing recursively

--- main.js
+++ main.js
@@ -6,17 +6,50 @@ canvas.height = SIZE;
 canvas.width = SIZE;
 
 ctx.strokeStyle = "white";
-ctx.lineWidth = "50px";
+ctx.lineWidth = 20;
 
 const sides = 5;
 
-ctx.translate(SIZE / 2, SIZE / 2);
+function drawFractal() {
+  ctx.save();
+  ctx.translate(SIZE / 2, SIZE / 2);
 
-for (let i = 0; i < sides; i++) {
+  for (let i = 0; i < sides; i++) {
+    drawBranch(0);
+
+    ctx.rotate((Math.PI * 2) / sides);
+  }
+  ctx.restore();
+}
+
+function drawBranch(level) {
+  if (level > 7) {
+    return;
+  }
+
+  const branchSize = 160;
+
   ctx.beginPath();
   ctx.moveTo(0, 0);
-  ctx.lineTo(200, 0);
+  ctx.lineTo(branchSize, 0);
   ctx.stroke();
 
-  ctx.rotate((Math.PI * 2) / sides);
+  ctx.save();
+
+  ctx.translate(branchSize, 0);
+  ctx.scale(0.6, 0.6);
+
+  ctx.save();
+  ctx.rotate(1.4);
+  drawBranch(level + 1);
+  ctx.restore();
+
+  ctx.save();
+  ctx.rotate(-1.4);
+  drawBranch(level + 1);
+  ctx.restore();
+
+  ctx.restore();
 }
+
+drawFractal();
