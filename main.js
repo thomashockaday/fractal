41b91321cd0fb3a92c78b1ddd9b41e2fdf905f05
Thomas Hockaday <thomashockaday@intelligencefusion.co.uk>
2026-01-15 15:48:53 +0000

    Define max level and spread values

--- main.js
+++ main.js
@@ -8,48 +8,43 @@ canvas.width = SIZE;
 ctx.strokeStyle = "white";
 ctx.lineWidth = 20;
 
-const sides = 5;
-
 function drawFractal() {
+  const sides = 5;
+  const maxLevel = 5;
+  const spread = Math.random() * 2 + 0.5;
+
   ctx.save();
   ctx.translate(SIZE / 2, SIZE / 2);
 
   for (let i = 0; i < sides; i++) {
-    drawBranch(0);
+    drawBranch(0, maxLevel, spread);
 
     ctx.rotate((Math.PI * 2) / sides);
   }
+
   ctx.restore();
 }
 
-function drawBranch(level) {
-  if (level > 7) {
+function drawBranch(level, maxLevel, spread) {
+  if (level > maxLevel) {
     return;
   }
 
-  const branchSize = 160;
+  const branchSize = 100;
 
   ctx.beginPath();
   ctx.moveTo(0, 0);
   ctx.lineTo(branchSize, 0);
   ctx.stroke();
 
-  ctx.save();
-
-  ctx.translate(branchSize, 0);
-  ctx.scale(0.6, 0.6);
-
-  ctx.save();
-  ctx.rotate(1.4);
-  drawBranch(level + 1);
-  ctx.restore();
-
-  ctx.save();
-  ctx.rotate(-1.4);
-  drawBranch(level + 1);
-  ctx.restore();
-
-  ctx.restore();
+  for (let i = 0; i < maxLevel; i++) {
+    ctx.save();
+    ctx.translate(branchSize, 0);
+    ctx.scale(0.7, 0.7);
+    ctx.rotate(spread);
+    drawBranch(level + 1, maxLevel, spread);
+    ctx.restore();
+  }
 }
 
 drawFractal();
