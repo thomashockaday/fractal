dfbbaedabd90c4482b6f4facbfd32ba566302486
Thomas Hockaday <thomashockaday@intelligencefusion.co.uk>
2026-01-15 17:07:02 +0000

    Add electricity draw mode

--- main.js
+++ main.js
@@ -63,6 +63,28 @@ class Fractal {
 
         ctx.globalAlpha = 1;
       }
+    } else if (this.drawMode === "electricity") {
+      if (level < this.maxLevel - 2) {
+        ctx.strokeStyle = `hsl(${this.hue}, 100%, ${lightness}%)`;
+
+        ctx.save();
+
+        ctx.lineWidth = 2;
+        ctx.shadowColor = `hsl(${this.hue}, 100%, 90%)`;
+        ctx.shadowBlur = 5;
+        ctx.shadowOffsetX = 0;
+        ctx.shadowOffsetY = 0;
+        ctx.beginPath();
+        ctx.moveTo(0, 0);
+        ctx.lineTo(this.branchSize * 0.2, Math.random() * 20 - 10);
+        ctx.lineTo(this.branchSize * 0.4, Math.random() * 50 - 25);
+        ctx.lineTo(this.branchSize * 0.6, Math.random() * 200 - 100);
+        ctx.lineTo(this.branchSize * 0.8, Math.random() * 100 - 50);
+        ctx.lineTo(this.branchSize, Math.random() * 40 - 20);
+        ctx.stroke();
+
+        ctx.restore();
+      }
     }
 
     for (let i = 0; i < this.branches; i++) {
@@ -88,6 +110,9 @@ function drawFractal() {
   fractal.draw(ctx);
 
   fractal.drawMode = "sparks";
+  fractal.draw(ctx);
+
+  fractal.drawMode = "electricity";
   fractal.draw(ctx);
 }
 
